9389689a0deef2919b1fda1b61f7d8a89ad37d1d
ValenGomezz <valentina.gomez.fara@gmail.com>
2025-10-25 22:15:58 -0300

    Metodo List de EjercicioAsignadoNegocio y DiaNegocio.

--- Business/DiaNegocio.cs
+++ Business/DiaNegocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,42 @@ using System.Threading.Tasks;
 
 namespace Business
 {
-    internal class DiaNegocio
+    public class DiaNegocio
     {
+        public List<Dia> listar()
+        {
+
+            List<Dia> lista = new List<Dia>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("select ID, RutinaID, NombreDía,  from Día");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Dia aux = new Dia();
+                    aux.Id = (int)datos.Lector["ID"];
+                    aux.RutinaId = (int)datos.Lector["RutinaID"];
+                    aux.NombreDia = (string)datos.Lector["NombreDía"];
+                    aux.Completado = (bool)datos.Lector["Completado"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
+}
 }
--- Business/EjercicioAsignadoNegocio.cs
+++ Business/EjercicioAsignadoNegocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,47 @@ using System.Threading.Tasks;
 
 namespace Business
 {
-    internal class EjercicioAsignadoNegocio
+    public class EjercicioAsignadoNegocio
     {
+        public List<EjercicioAsignado> listar()
+        {
 
+            List<EjercicioAsignado> lista = new List<EjercicioAsignado>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("select ID, DíaID, EjercicioBaseID, Series, Repeticiones, TiempoEstimado, Peso, Observaciones, Url from EjercicioAsignado");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    EjercicioAsignado aux = new EjercicioAsignado();
+                    aux.Id = (int)datos.Lector["ID"];
+                    aux.DiaId = (int)datos.Lector["DíaID"];
+                    aux.EjercicioBaseId = (int)datos.Lector["EjercicioBaseID"];
+                    aux.Series = (int)datos.Lector["Series"];
+                    aux.Repeticiones = (int)datos.Lector["Repeticiones"];
+                    aux.TiempoEstimado = (int)datos.Lector["TiempoEstimado"];
+                    aux.Peso = (decimal)datos.Lector["Peso"];
+                    aux.Observaciones = (string)datos.Lector["Observaciones"];
+                    aux.Url = (string)datos.Lector["Url"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
+}
 }
