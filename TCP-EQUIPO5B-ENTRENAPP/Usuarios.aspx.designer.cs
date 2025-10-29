68ceea060c77c4adb2afd890af25bfdb9df6d845
ValenGomezz <valentina.gomez.fara@gmail.com>
2025-10-29 16:20:38 -0300

    check box y button en formulario de registro de usuario

--- TCP-EQUIPO5B-ENTRENAPP/Usuarios.aspx.designer.cs
+++ TCP-EQUIPO5B-ENTRENAPP/Usuarios.aspx.designer.cs
@@ -67,5 +67,23 @@ namespace TCP_EQUIPO5B_ENTRENAPP
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtTelefono;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control CheckBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox CheckBox1;
     }
 }
