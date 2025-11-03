bd76c0be6a6422da940925ad50d498be4ca5cee0
brunobberti <brunoberti094@gmail.com>
2025-11-02 23:44:36 -0300

    modificar y eliminar de Ejercicio Base

--- Business/EjercicioBaseNegocio.cs
+++ Business/EjercicioBaseNegocio.cs
@@ -73,6 +73,52 @@ namespace Business
 
 
         }
+
+        public void modificar(EjercicioBase ejercicioBaseModificado)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+
+                
+                datos.setearConsulta("update EjercicioBase set Nombre = @nombre, Descripción = @desc, Url=@url WHERE ID = @id;");
+                datos.setearParametro("@nombre",ejercicioBaseModificado.Nombre);
+                datos.setearParametro("@desc", ejercicioBaseModificado.Descripcion);
+                datos.setearParametro("@url", ejercicioBaseModificado.Url);
+                datos.setearParametro("@id", ejercicioBaseModificado.Id);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminar( int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("DELETE FROM EjercicioBase WHERE id = @id");
+                datos.setearParametro("@id",id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
 
 }
