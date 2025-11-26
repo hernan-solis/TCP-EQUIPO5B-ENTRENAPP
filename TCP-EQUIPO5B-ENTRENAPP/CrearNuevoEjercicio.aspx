e75312215f7c203ff2f415a9631bc84b81672478
brunobberti <brunoberti094@gmail.com>
2025-11-26 19:38:46 -0300

    Se crea la ventana para crear nuevo ejercicio base con la funcionalidad para agregarlo OK

---
+++ TCP-EQUIPO5B-ENTRENAPP/CrearNuevoEjercicio.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CrearNuevoEjercicio.aspx.cs" Inherits="TCP_EQUIPO5B_ENTRENAPP.CrearNuevoEjercicio" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="text-center" aria-labelledby="gettingStartedTitle">
+        <h4>Nuevo Ejercicio</h4>
+
+    </div>
+    <div class="row">
+        <div class="col-6">
+            <label class="form-label">Nombre</label>
+            <asp:TextBox ID="txtNombre" CssClass="form-control" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator
+                ID="rfvNombre"
+                runat="server"
+                ControlToValidate="txtNombre"
+                ErrorMessage="Campo obligatorio"
+                Text="Campo obligatorio *"
+                ForeColor="Red"
+                Display="Dynamic" />
+            <asp:RegularExpressionValidator
+                ID="revNombre"
+                runat="server"
+                ControlToValidate="txtNombre"
+                ValidationExpression="^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{3,}$"
+                ErrorMessage="* El nombre solo puede contener letras y debe tener al menos 3 caracteres."
+                Text="* El nombre solo puede contener letras y debe tener al menos 3 caracteres."
+                ForeColor="Red"
+                Display="Dynamic" />
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Descripcion</label>
+            <asp:TextBox ID="txtDescripcion" CssClass="form-control" runat="server"></asp:TextBox>
+
+        </div>
+        <div class="mb-3">
+            <label class="form-label">URL</label>
+            <asp:TextBox ID="txtURL" CssClass="form-control" runat="server"></asp:TextBox>
+
+        </div>
+        <section>
+
+            <asp:Button ID="btnAgregar" runat="server" class="btn btn-primary btn-lg text-uppercase" Text="Agregar" OnClick="BtnAgregar_Click" OnClientClick="return confirm('¿Está seguro que desea Guardar este ejercicio?');" />
+        </section>
+
+
+    </div>
+
+
+</asp:Content>
---
+++ TCP-EQUIPO5B-ENTRENAPP/CrearNuevoEjercicio.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TCP_EQUIPO5B_ENTRENAPP
+{
+
+
+    public partial class CrearNuevoEjercicio
+    {
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control rfvNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvNombre;
+
+        /// <summary>
+        /// Control revNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator revNombre;
+
+        /// <summary>
+        /// Control txtDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDescripcion;
+
+        /// <summary>
+        /// Control txtURL.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtURL;
+
+        /// <summary>
+        /// Control btnAgregar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregar;
+    }
+}
